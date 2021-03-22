9d5aa3f28bee52bf6ae2fe6d2ce13debb028e28f
Marta Kuśpit <mkuspit@gmail.com>
2021-03-22 08:14:37 +0100

    oninit off

--- src/main/java/my/project/userAccess/DbInit.java
+++ src/main/java/my/project/userAccess/DbInit.java
@@ -2,31 +2,31 @@ package my.project.userAccess;
 
 
 
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-
-@Component
-public class DbInit {
-    private UserRepository userRepository;
-    private PasswordEncoder passwordEncoder;
-
-    public DbInit(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @PostConstruct
-    public void onInit() {
-
-        User user1 = new User(
-                "admin1",
-                passwordEncoder.encode("admin"),
-                "ADMIN"
-        );
-        userRepository.save(user1);
-    }
-
-}
-
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Component;
+//
+//import javax.annotation.PostConstruct;
+//
+//@Component
+//public class DbInit {
+//    private UserRepository userRepository;
+//    private PasswordEncoder passwordEncoder;
+//
+//    public DbInit(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+//        this.userRepository = userRepository;
+//        this.passwordEncoder = passwordEncoder;
+//    }
+//
+//    @PostConstruct
+//    public void onInit() {
+//
+//        User user1 = new User(
+//                "admin1",
+//                passwordEncoder.encode("admin"),
+//                "ADMIN"
+//        );
+//        userRepository.save(user1);
+//    }
+//
+//}
+//
