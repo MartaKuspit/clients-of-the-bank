1cba51224fa4ac5aa576f62291def2e18b2e1978
Marta Kuśpit <mkuspit@gmail.com>
2021-03-22 13:43:41 +0100

    czesciow przywr

--- src/main/java/my/project/userAccess/User.java
+++ src/main/java/my/project/userAccess/User.java
@@ -1,74 +1,74 @@
-//package my.project.userAccess;
-//
-//
+package my.project.userAccess;
+
+
 //import org.springframework.security.core.GrantedAuthority;
 //import org.springframework.security.core.authority.SimpleGrantedAuthority;
 //import org.springframework.security.core.userdetails.UserDetails;
-//
-//import javax.persistence.*;
-//import javax.validation.constraints.NotEmpty;
-//import java.util.Collection;
-//import java.util.Collections;
-//
-//@Entity
-//@Table(name = "app_users")
-//public class User implements UserDetails {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    @NotEmpty
-//    private String username;
-//    @NotEmpty
-//    private String password;
-//    @NotEmpty
-//    private String role;
-//
-//    public void setRole(String role) {
-//        this.role = role;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public User(String username, String role) {
-//        this.username = username;
-//        this.role = role;
-//    }
-//
-//    public User(String username, String password,  String role) {
-//        this.username = username;
-//        this.password = password;
-//        this.role = role;
-//    }
-//
-//    public User() {
-//
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity
+@Table(name = "app_users")
+public class User  { //implements UserDetails
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String username;
+    @NotEmpty
+    private String password;
+    @NotEmpty
+    private String role;
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User(String username, String role) {
+        this.username = username;
+        this.role = role;
+    }
+
+    public User(String username, String password,  String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User() {
+
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 //    @Override
 //    public Collection<? extends GrantedAuthority> getAuthorities() {
 //        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + role));
 //    }
-//
-//    public String getRole() {
-//        return role;
-//    }
-//
-//    @Override
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return username;
-//    }
-//
+
+    public String getRole() {
+        return role;
+    }
+
+    //@Override
+    public String getPassword() {
+        return password;
+    }
+
+    //@Override
+    public String getUsername() {
+        return username;
+    }
+
 //    @Override
 //    public boolean isAccountNonExpired() {
 //        return true;
@@ -88,4 +88,4 @@
 //    public boolean isEnabled() {
 //        return true;
 //    }
-//}
+}
