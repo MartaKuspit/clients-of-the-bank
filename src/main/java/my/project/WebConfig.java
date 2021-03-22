6f65d5580c402925ff2b7082d6d3a91426b5e2ba
Marta Kuśpit <mkuspit@gmail.com>
2021-03-22 14:20:49 +0100

    web

--- src/main/java/my/project/WebConfig.java
+++ src/main/java/my/project/WebConfig.java
@@ -1,4 +1,4 @@
-package com.programming.techie.springredditclone.config;
+package my.project;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
